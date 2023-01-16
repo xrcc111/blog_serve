41b2162e5613533026cd0badd79d45975f095169
徐锐 <2603029264@qq.com>
2023-01-17 00:28:57 +0800

    添加去重sql,问题有待修改

--- controllers/common.js
+++ controllers/common.js
@@ -20,6 +20,10 @@
     const connection = ctx.request.header['Connection']
     const sql = `insert into client values (null, '${connection}', "${clientIP}", "${user_agent}", now()) `
     const result = await query(sql)
+    // 简单去重保证数据不冗余 ,后期考虑做联合查询省略此处步骤
+    const sql2 = `delete p1 from client as p1,client as p2
+    where p1.create_time != p2.create_time and p1.user_ip = p2.user_ip and p1.id > p2.id;`
+    const res = await query(sql2)
     success(ctx, {
       message: 'ok'
     })
